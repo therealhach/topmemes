81cf0e723de57e3392259ac8c74690b9452d2b79
therealhach <irvinabarca.nv@gmail.com>
2025-11-26 10:41:54 +0800

    updating favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,9 +87,10 @@ export const metadata: Metadata = {
     telephone: false,
   },
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    icon: [
+      { url: "/icon.svg", type: "image/svg+xml" },
+    ],
+    apple: "/icon.svg",
   },
   manifest: "/site.webmanifest",
 };
